fe1a085504fe4f0f002d3dae867289d537ab0192
Ian Luckraft <ian.luckraft@headscape.co.uk>
2020-06-16 18:35:57 +0100

    Tidy up the messages

--- src/components/SearchProfile.vue
+++ src/components/SearchProfile.vue
@@ -8,9 +8,9 @@
                     <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"/>
                 </svg>
                 </button>
-                <p v-if="this.error" class="pt-4 text-red-400">An Instagram profile with that username could not be found</p>
-                <div v-if="this.profile" class="text-center pt-4">
-                    <p class="text-sm pb-4">Scroll down to view the profile</p>
+                <div class="text-center pt-4 pb-4">
+                    <p v-if="this.error" class="text-red-400">An Instagram profile with that username could not be found</p>
+                    <p v-if="this.profile">Scroll down to view the profile</p>
                 </div>
             </form>
         </div>
