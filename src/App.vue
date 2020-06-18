da6228814785b01fabb33d6c18b44fb09c15d360
Ian Luckraft <ian.luckraft@headscape.co.uk>
2020-06-18 20:45:34 +0100

    Output the most liked post

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <div v-if="Object.keys(this.$store.getters.porfileData).length > 0">
       <UserProfile />
       <UserPost title="Most Recent" :post="mostRecentPost" />
+      <UserPost title="Most Liked" :post="mostLikedPost" />
     </div>
   </div>
 </template>
@@ -23,7 +24,17 @@ export default {
   computed: {
     mostRecentPost: function() {
       return this.$store.getters.porfileData.user.edge_owner_to_timeline_media.edges[0];
-    }
+    },
+    mostLikedPost: function() {
+      var mostLikedKey = 0;
+      var likesCount = 0;
+      this.$store.getters.porfileData.user.edge_owner_to_timeline_media.edges.forEach(function (item, index) {
+        if (item.node.edge_liked_by.count > likesCount) {
+          mostLikedKey = index;
+        }
+      });
+      return this.$store.getters.porfileData.user.edge_owner_to_timeline_media.edges[mostLikedKey];
+    },
   }
 }
 </script>
