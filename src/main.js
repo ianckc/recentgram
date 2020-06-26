d90dbe3a8a13356d1c1e3cd30115d5750981200d
Ian Luckraft <ian.luckraft@headscape.co.uk>
2020-06-26 21:25:25 +0100

    Add AOS

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import { store } from './store/store'
 import './assets/styles/main.css'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 Vue.config.productionTip = false
 
@@ -9,5 +11,8 @@ Vue.use(require('vue-moment'));
 
 new Vue({
   store,
+  created () {
+    AOS.init()
+  },
   render: h => h(App),
 }).$mount('#app')
